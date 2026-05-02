2b6412c4f8bc7a14e3b02afa132e177095f7c714
aref1996deeb-create <aref1996deeb@gmail.com>
2026-05-02 17:56:37 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
 };
 
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-};
-
->>>>>>> a0cbb82 (تجهيز ملفات المتجر ونظام الطلبات - بواسطة عارف ديب)
 export default nextConfig;
